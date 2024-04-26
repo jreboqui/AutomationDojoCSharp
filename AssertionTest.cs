aaef15b0c9f1d22393b4e7f6cecee7a7ee4b973e
Junnel Reboquio <jreboqui@gmail.com>
2024-04-26 11:00:31 -0700

    Added some sample tests

---
+++ AssertionTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaiPractice;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class AssertOnThings : PageTest {
+    [Test]
+    public async Task ThisTestGoesToAPageAndAsserts() {
+        await Page.GotoAsync("https://commitquality.com");
+    }
+}
+
